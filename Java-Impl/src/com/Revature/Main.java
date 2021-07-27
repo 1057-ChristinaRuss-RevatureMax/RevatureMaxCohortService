ed4523da46f9b57e0bb909b7179af263d5a12a47
Raymond Amparo <ray.amparo1995@gmail.com>
2021-07-27 14:01:14 -0700

    API test

--- Java-Impl/src/com/Revature/Main.java
+++ Java-Impl/src/com/Revature/Main.java
@@ -1,8 +1,38 @@
 package com.Revature;
+import java.net.URLEncoder;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        try {
+            URL url = new URL("https://caliber2-mock.revaturelabs.com/mock/training/associate");
+            HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+            int responseCode = conn.getResponseCode();
+            System.out.println(responseCode);
+            if (conn.getResponseCode() != 200) {
+                throw new RuntimeException("Failed : HTTP Error code : "
+                        + conn.getResponseCode());
+            } else {
+                InputStreamReader in = new InputStreamReader(conn.getInputStream());
+                BufferedReader br = new BufferedReader(in);
+                String output;
+                while ((output = br.readLine()) != null) {
+                    System.out.println(output);
+                }
+                conn.disconnect();
+            }
+        } catch(Exception e) {
+            System.out.println("Exception in NetClientGet:- " + e);
+        }
+
     }
 }
--- Java-Impl/src/dbconfig/ConnectionConfig.java
+++ Java-Impl/src/dbconfig/ConnectionConfig.java
@@ -1,57 +1,57 @@
 package dbconfig;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.DriverManager;
-
-public class ConnectionConfig {
-
-    private Connection dbConnection;
-
-    public static Connection getConnection() throws SQLException {
-        Connection con = null;
-        try {
-            Class.forName("org.postgresql.Driver");
-            String hostname =  System.getenv("AWS_HOST_NAME");
-            String userName = System.getenv("DB_USER");
-            String password = System.getenv("DB_PASS");
-            String port = "5432";
-            String dbName = System.getenv("DB_NAME");
-
-
-            // Parameters for setting up the jdbcURL loaded in to connect to database currently set as postgres (anyone can access)
-            String jdbcUrl = "jdbc:postgresql://" + hostname + ":" + port + "/" + dbName + "?user=" + userName + "&password=" + password;
-            // jdbc setup for connection url to pass as a whole
-            con = DriverManager.getConnection(jdbcUrl);
-            return con;
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    public static Connection getTestConnection() throws SQLException {
-        Connection con = null;
-        try {
-            Class.forName("org.postgresql.Driver");
-            String hostname =  "localhost";
-            String port = "5432";
-            String userName = System.getenv("TEST_DB_USER");
-            String password = System.getenv("TEST_DB_PASS");
-            String dbName = System.getenv("DB_NAME");
-            // Parameters for setting up the jdbcURL loaded in to connect to database currently set as postgres (anyone can access)
-            String jdbcUrl = "jdbc:postgresql://" + hostname + ":" + port + "/" + dbName + "?user=" + userName + "&password=" + password;
-            // jdbc setup for connection url to pass as a whole
-            con = DriverManager.getConnection(jdbcUrl);
-            // store connection to database in a variable
-            return con;
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    public int executeQuery(String query) throws ClassNotFoundException, SQLException {
-        return dbConnection.createStatement().executeUpdate(query);
-    }
-}
+//
+//import java.sql.Connection;
+//import java.sql.SQLException;
+//import java.sql.DriverManager;
+//
+//public class ConnectionConfig {
+//
+//    private Connection dbConnection;
+//
+//    public static Connection getConnection() throws SQLException {
+//        Connection con = null;
+//        try {
+//            Class.forName("org.postgresql.Driver");
+//            String hostname =  System.getenv("AWS_HOST_NAME");
+//            String userName = System.getenv("DB_USER");
+//            String password = System.getenv("DB_PASS");
+//            String port = "5432";
+//            String dbName = System.getenv("DB_NAME");
+//
+//
+//            // Parameters for setting up the jdbcURL loaded in to connect to database currently set as postgres (anyone can access)
+//            String jdbcUrl = "jdbc:postgresql://" + hostname + ":" + port + "/" + dbName + "?user=" + userName + "&password=" + password;
+//            // jdbc setup for connection url to pass as a whole
+//            con = DriverManager.getConnection(jdbcUrl);
+//            return con;
+//        } catch (ClassNotFoundException | SQLException e) {
+//            e.printStackTrace();
+//            return null;
+//        }
+//    }
+//
+//    public static Connection getTestConnection() throws SQLException {
+//        Connection con = null;
+//        try {
+//            Class.forName("org.postgresql.Driver");
+//            String hostname =  "localhost";
+//            String port = "5432";
+//            String userName = System.getenv("TEST_DB_USER");
+//            String password = System.getenv("TEST_DB_PASS");
+//            String dbName = System.getenv("DB_NAME");
+//            // Parameters for setting up the jdbcURL loaded in to connect to database currently set as postgres (anyone can access)
+//            String jdbcUrl = "jdbc:postgresql://" + hostname + ":" + port + "/" + dbName + "?user=" + userName + "&password=" + password;
+//            // jdbc setup for connection url to pass as a whole
+//            con = DriverManager.getConnection(jdbcUrl);
+//            // store connection to database in a variable
+//            return con;
+//        } catch (ClassNotFoundException | SQLException e) {
+//            e.printStackTrace();
+//            return null;
+//        }
+//    }
+//
+//    public int executeQuery(String query) throws ClassNotFoundException, SQLException {
+//        return dbConnection.createStatement().executeUpdate(query);
+//    }
+//}
