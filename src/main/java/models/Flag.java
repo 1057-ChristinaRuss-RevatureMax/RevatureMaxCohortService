7f2f70af027737f78c780316ea173605cbdeb91d
KyoldFusion <michaeltyler96@icloud.com>
2021-07-28 13:38:12 -0400

    Model Standards Created
    models created now need to implement JSON parsing

---
+++ src/main/java/models/Flag.java
@@ -0,0 +1,46 @@
+package models;
+
+public class Flag {
+    private String content;
+    private int id;
+    private String type;
+
+    public Flag(String content, int id, String type) {
+        this.content = content;
+        this.id = id;
+        this.type = type;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "content=':'" + content + '\'' +
+                ", id=':'" + id +
+                ", type=':'" + type + '\'' +
+                '}';
+    }
+}
