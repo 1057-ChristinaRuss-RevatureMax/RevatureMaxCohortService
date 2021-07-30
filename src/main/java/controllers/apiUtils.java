3ddb5479d4cad7a503238edb807974ce2be0ee16
tgrossman14 <88042455+tgrossman14@users.noreply.github.com>
2021-07-30 12:34:45 -0400

    Resolved merges

--- src/main/java/controllers/apiUtils.java
+++ src/main/java/controllers/apiUtils.java
@@ -5,6 +5,7 @@ import models.*;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class apiUtils {
     public static ArrayList<Associate> JSONConvertAssociate(String paramBody) {
@@ -251,4 +252,29 @@ public class apiUtils {
         );
         return flag;
     }
+
+    public static HashMap<String, String> mapFromJson(String json) {
+        HashMap<String, String> paramMap = new HashMap<String, String>();
+
+        if (json.length() > 2)
+            json = json.substring(1, json.length() - 1);
+        else
+            return paramMap;
+
+        String[] bodyList = json.split(",");
+        for (int i = 0; i < bodyList.length; i++) {
+            String[] params = bodyList[i].split(":");
+            if (params.length > 1) {
+                String key = params[0];
+                String value = params[1];
+                if (key.length() > 2 && value.length() > 2) {
+                    key = key.substring(1, key.length() - 1);
+                    value = value.substring(1, value.length() - 1);
+                    paramMap.put(key, value);
+                }
+            }
+        }
+        return paramMap;
+    }
+
 }
--- src/main/java/driver/Main.java
+++ src/main/java/driver/Main.java
@@ -1,10 +1,18 @@
 package driver;
 
-
 //import config.JavalinConfig;
 //import frontcontroller.FrontController;
 //import io.javalin.Javalin;
+import config.JavalinConfig;
+import frontcontroller.FrontController;
+import io.javalin.Javalin;
 
 public class Main {
+    public static void main(String[] args) {
+        //Javalin app = Javalin.create().start(9001);
+        Javalin app = JavalinConfig.getApp(9001);
 
+        FrontController fc = new FrontController(app);
+    }
 }
+
