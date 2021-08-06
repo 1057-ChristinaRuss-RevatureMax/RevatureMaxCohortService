b08e1bdcbdfc61c22c35ebf93a102b6518f67475
TheJackalope24 <59453093+TheJackalope24@users.noreply.github.com>
2021-08-06 12:11:30 -0400

    Update Script.sql

--- scripts/Script.sql
+++ scripts/Script.sql
@@ -1,6 +1,6 @@
 
 drop TABLE IF exists associate_portfolio;
-drop TABLE IF exists associate_portfolio;
+drop TABLE IF exists employee_portfolio;
 drop TABLE IF exists associate;
 drop table if exists employee;
 
@@ -9,40 +9,39 @@ create table associate(
     salesforceId varchar(10) primary key not null, 
     firstname varchar(20) not null,
     lastname varchar(20) not null,
-    email varchar(80),
+    email varchar(70),
     pswrd varchar(40)
 );
 
 
 create table associate_portfolio(
+	salesforceId varchar(10) primary key not null,
     bio text,
     favorite_technologies text,
     preference varchar(15),
-    salesforceId varchar(10) not null,
     foreign key (salesforceId)
     references associate(salesforceId)
 
 );
 
 create table employee (
-    salesforceId Integer primary key not null, 
+    salesforceId INTEGER primary key not null, 
     firstname varchar(20) not null,
     lastname varchar(20) not null,
-    email varchar(80),
+    email varchar(70),
     pswrd varchar(40)
 );
 
 
 
 create table employee_portfolio(
+	salesforceId INTEGER primary key not null,
     bio text,
     technology text,
     trainer_location varchar(25),
-    salesforceId Integer not null,
     foreign key (salesforceId)
-    references associate(salesforceId)
+    references employee(salesforceId)
     
 );
-
 
      
