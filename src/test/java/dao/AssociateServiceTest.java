a5bb95b5731422bc7614a7a1464d4b8fd0f90e87
KyoldFusion <michaeltyler96@icloud.com>
2021-08-11 16:11:02 -0400

    Update AssociateServiceTest.java

--- src/test/java/dao/AssociateServiceTest.java
+++ src/test/java/dao/AssociateServiceTest.java
@@ -11,7 +11,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class AssociatePortfolioDaoTest {
+public class AssociateServiceTest {
 
     @Test(groups = {"requireDB"})
     public void testUpdateBio(){
